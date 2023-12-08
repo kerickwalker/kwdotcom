3ed40cd326b68416116b2aed7e4a4f710e7edebf
Kerick Walker <kerickwalker@gmail.com>
2023-12-08 22:30:54 +0100

    add inverse animations for contracting the navbar

--- assets/javascript/animations.js
+++ assets/javascript/animations.js
@@ -1,12 +1,23 @@
+clicks = 0;
+
 function navbarClick() {
-    console.log('clicked');
-    glowContainer = document.getElementById('glow-container');
-    glowContainer.classList.add('glow-spread');
-    console.log(glowContainer.classList);
-    about = document.getElementById('about');
-    about.classList.add('display-about');
-    projects = document.getElementById('projects');
-    projects.classList.add('display-projects');
-    links = document.getElementById('links');
-    links.classList.add('display-links');
+        glowContainer = document.getElementById('glow-container');
+        about = document.getElementById('about');
+        projects = document.getElementById('projects');
+        links = document.getElementById('links');
+
+        if(clicks%2 == 0){
+            glowContainer.style.animation = 'glowSpread 3s forwards';
+            about.style.animation = 'displayRects 1.0s 0.5s forwards';
+            projects.style.animation = 'displayRects 1.0s 1.0s forwards';
+            links.style.animation = 'displayRects 1.0s 2.5s forwards';
+            clicks = clicks + 1;
+        }
+        else{
+            glowContainer.style.animation = 'glowShrink 3s forwards';
+            links.style.animation = 'hideRects 0.7s forwards';
+            projects.style.animation = 'hideRects 1.5s forwards';
+            about.style.animation = 'hideRects 3.0s forwards';
+            clicks = clicks +1;
+        }
 }
