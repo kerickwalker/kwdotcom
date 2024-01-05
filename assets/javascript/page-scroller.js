cd478fd5f95360e341692b88163da9b0db62ca40
Kerick Walker <kerickwalker@gmail.com>
2024-01-04 21:59:54 -0800

    add scrolling functionality

---
+++ assets/javascript/page-scroller.js
@@ -0,0 +1,11 @@
+function aboutScroll() {
+    window.scrollTo(0, window.innerHeight / 100 * 85);
+}
+
+function projectsScroll() {
+    window.scrollTo(0, window.innerHeight / 100 * 170);
+}
+
+function linksScroll() {
+    window.scrollTo(0, window.innerHeight / 100 * 255);
+}
